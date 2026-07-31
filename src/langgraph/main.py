68907272e57877826e3e51e8b1f4920975274db2
ShaanthiSwarupp <shaanthiswarupp@gmail.com>
2026-07-31 15:19:31 +0530

    first commit

---
+++ src/langgraph/main.py
@@ -0,0 +1,57 @@
+import streamlit as st
+
+from src.langgraph.UI.streamlitUi.LoadUi import LoadStreamlitUi
+
+# from src.langgraph.graph.graph_builder import GraphBuilder
+
+def langgraph_app():
+    """ Loads and runs the LangGraph AgenticAI application with Streamlit UI.
+
+        This function initializes the UI, handles user input, configures the LLM model,
+
+        sets up the graph based on the selected use case, 
+
+        and displays the output while implementing exception handling for robustness. 
+    """
+
+    ui = LoadStreamlitUi() ## Load the Streamlit UI
+    user_input = ui.load_ui() ## Load the user controls from the UI
+
+    if not user_input:
+        st.error("Error: Failed to load user input from the UI.")
+        return
+
+    user_message = st.chat_input("Enter your message:")
+
+    """ if user_message:
+        try: 
+            ##================== model=================
+            obj_llm_config = GroqLLM(user_controls_input = user_input) ## Configure the LLM model based on user input
+            model = obj_llm_config.get_llm_model() ## Configure the model
+
+            if not model:
+                st.error("Error: LLM model could not be initialized")
+                return
+
+            ##================== useCase =================
+            usecase = user_input.get("selected_usecase")
+
+            if not usecase:
+                st.error("Error: No use case selected.")
+                return
+
+            ##================== graph Builder  =================
+            graph_builder = GraphBuilder(model)
+            try: 
+                graph = graph_builder.setup_graph(usecase)
+                print(user_message)
+                displayResultStreamlit(usecase,graph,user_message).display_results_on_ui()
+
+            except Exception as e :
+                st.error(f"Error: graph set up faiild - {e} ")
+                return
+
+        except Exception as e:
+            st.error(f"Error configuring the LLM model: {e}")
+            return  
+    """
