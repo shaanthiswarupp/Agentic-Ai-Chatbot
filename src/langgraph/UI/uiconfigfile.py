5aa8d18bbe981f3cfd3a18ebdef0ccd3a837c2ea
ShaanthiSwarupp <shaanthiswarupp@gmail.com>
2026-07-31 12:53:34 +0530

    first commit

--- src/langgraph/UI/uiconfigfile.py
+++ src/langgraph/UI/uiconfigfile.py
@@ -1,7 +1,22 @@
 from configparser import ConfigParser
 
 class config:
-    def __init__(self, configfile = "src/langgraph/UI/uiconfigfile.ini"):
+
+    def __init__(self, configfile = "./src/langgraph/UI/uiconfigfile.ini"):
+
         self.config = ConfigParser()
         self.config.read(configfile)
-        
+
+
+
+    def get_page_title(self):
+        return self.config.get("DEFAULT", "PAGE_TITLE")
+
+    def get_llms_options(self):
+        return self.config.get("DEFAULT", "LLMS_OPTIONS").split(", ")
+
+    def get_groq_models_options(self):
+        return self.config.get("DEFAULT", "GROQ_MODELS_OPTIONS").split(", ")
+
+    def get_usecases_options(self):
+        return self.config.get("DEFAULT", "USECASES_OPTIONS").split(", ")
