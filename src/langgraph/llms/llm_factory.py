b7142be10ba9b30633a4df4a22d5d99d196493f2
Shaanthi Swarupp <154413447+shaanthiswarupp@users.noreply.github.com>
2026-09-11 19:47:36 +0530

    Enhance API key retrieval for Groq and OpenAI

--- src/langgraph/llms/llm_factory.py
+++ src/langgraph/llms/llm_factory.py
@@ -15,7 +15,7 @@ class LLMFactory:
 
         try:
             if provider == "Groq":
-                groq_api_key = self.user_controls_input["GROQ_API_KEY"]
+                groq_api_key = self.user_controls_input["GROQ_API_KEY"] or st.secrets.get("GROQ_API_KEY", "") or os.environ.get("GROQ_API_KEY", "")
                 selected_groq_model=self.user_controls_input["selected_groq_model"]
 
                 if groq_api_key=='' and os.environ["GROQ_API_KEY"] =='':
@@ -24,7 +24,7 @@ class LLMFactory:
                 llm = ChatGroq(api_key=groq_api_key, model=selected_groq_model)
                 
             elif provider == "OpenAI":
-                openai_api_key = self.user_controls_input["OPENAI_API_KEY"]
+                openai_api_key = self.user_controls_input["OPENAI_API_KEY"] or st.secrets.get("OPENAI_API_KEY", "") or os.environ.get("OPENAI_API_KEY", "")
                 selected_openai_model = self.user_controls_input["selected_openai_model"]
 
                 if openai_api_key == '' and os.environ["OPENAI_API_KEY"] == '':
